3c8e594af3582b7e2e3d9ff8a3ce088a634d3437
unknown <kiyukrn@gmail.com>
2024-12-14 20:34:24 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
+import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 
 const App = () => {
